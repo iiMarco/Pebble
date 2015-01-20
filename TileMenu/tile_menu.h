d6cb7dab7f958a5c566b65b3c2f1f339d28aab1b
Mark Zammit <iimarco@me.com>
2015-01-20 12:32:03 +1100

    Update tile_menu.h
    Added tile selector functionality.

--- TileMenu/tile_menu.h
+++ TileMenu/tile_menu.h
@@ -1,4 +1,5 @@
 #include <pebble.h>
+#include "animator.h"
 
 typedef struct _tile_menu_ TileMenu;
 
@@ -7,13 +8,17 @@ void            tile_menu_destroy(TileMenu * menu);
 
 void            tile_menu_draw(TileMenu * menu);
 
-GRect           tile_menu_get_bounds(TileMenu * menu);
 Layer *         tile_menu_get_next(TileMenu * menu);
 Layer *         tile_menu_get_prev(TileMenu * menu);
 Layer *         tile_menu_get_curr(TileMenu * menu);
+bool            tile_menu_at_end(TileMenu * menu);
+bool            tile_menu_at_begin(TileMenu * menu);
+
+GRect           tile_menu_get_bounds(TileMenu * menu);
 int             tile_menu_get_tile_count(TileMenu * menu);
 Window *        tile_menu_get_window(TileMenu * menu);
 Layer *         tile_menu_get_layer(TileMenu * menu);
 
-bool            tile_menu_at_end(TileMenu * menu);
-bool            tile_menu_at_begin(TileMenu * menu);
+Layer *         tile_menu_get_selected(TileMenu * menu);
+void            tile_menu_set_selected_next(TileMenu * menu);
+void            tile_menu_set_selected_prev(TileMenu * menu);
