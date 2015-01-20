1caf959a1a9a9a2ee8b91c095a53200dbb206cae
Mark Zammit <iimarco@me.com>
2015-01-20 12:33:31 +1100

    Create animator.c

---
+++ TileMenu/animator.c
@@ -0,0 +1,26 @@
+#include <pebble.h>
+#include "animator.h"
+
+void on_animation_stopped(Animation *anim, bool finished, void *context) {
+    //Free the memoery used by the Animation
+    property_animation_destroy((PropertyAnimation*) anim);
+}
+
+void animate_layer(Layer *layer, GRect *start, GRect *finish, int duration, int delay) {
+    //Declare animation
+    PropertyAnimation *anim = property_animation_create_layer_frame(layer, start, finish);
+ 
+    //Set characteristics
+    animation_set_duration((Animation*) anim, duration);
+    animation_set_delay((Animation*) anim, delay);
+ 
+    //Set stopped handler to free memory
+    AnimationHandlers handlers = {
+        //The reference to the stopped handler is the only one in the array
+        .stopped = (AnimationStoppedHandler) on_animation_stopped
+    };
+    animation_set_handlers((Animation*) anim, handlers, NULL);
+ 
+    //Start animation!
+    animation_schedule((Animation*) anim);
+}
